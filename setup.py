97b0d2317592fdc1c1c64f88b88c7054b0ef50bc
Micah Johnson <micah.johnson150@gmail.com>
2020-04-29 09:42:09 -0600

    Bump version: 0.14.5 → 0.14.6

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/USDA-ARS-NWRC/basin_setup',
-    version='0.14.5',
+    version='0.14.6',
     zip_safe=False,
 )
