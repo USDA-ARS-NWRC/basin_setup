8afba0cfa86600b19f7f8b787b54e1473a8dbe6a
Scott Havens <scott.havens@ars.usda.gov>
2021-07-19 12:52:41 -0600

    cleaning up

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
     packages=find_packages(include=['basin_setup', 'basin_setup.*']),
     entry_points={
         'console_scripts': [
-            'basin_setup=basin_setup.generate_topo.basin_setup_old:main',
             'generate_topo=basin_setup.cli.generate_topo:main',
             'delineate=basin_setup.delineate:main',
             'grm=basin_setup.grm:main',
